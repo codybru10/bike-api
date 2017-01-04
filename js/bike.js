191f82547542e00e85607bdf6b5cb726490eb6bc
Cody Brubaker and Marty Kovach <cody+marty@>
2017-01-04 15:46:24 -0800

    add 'nothing has been stolen' message

--- js/bike.js
+++ js/bike.js
@@ -24,17 +24,17 @@ Bike.prototype.showStolen = function(location) {
 Bike.prototype.justStolen = function(location) {
   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=1000&location=' + location + '&distance=10&stolenness=proximity&appid=' + apiKey).then(function(response) {
     console.log(response);
-    debugger;
       var today = new Date();
-      var lastWeek = today.setDate(today.getDate()-15);
+      var lastWeek = today.setDate(today.getDate()-7);
       var lastWeek = lastWeek / 1000;
       response.bikes.forEach(function(bike){
         if(bike.date_stolen > lastWeek) {
           console.log("WAHHT");
             $('#recentList').append("<li>" + bike.title + " "+ bike.date_stolen + "</li>");
-        }
-      });
-
+        } else {
+          $('#recentList').text("Nothing has been stolen in the last 7 days");
+      };
+    });
   });
 };
 
