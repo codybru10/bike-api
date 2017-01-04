53aa000bedd397a26213f54e90dc66f4e171e421
Cody Brubaker and Marty Kovach <cody@>
2017-01-04 15:17:15 -0800

    displays number of stolen bikes, lists makes of stolen bikes, and lists bikes stolen within the last week.

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,14 +1,15 @@
  var apiKey = require('./../.env').apiKey;
  var Bike = require('./../js/bike.js').bikeModule;
-
+ var appId = require('./../.env').appId;
 $(document).ready(function(){
+  // $.post('https://bikeindex.org/oauth/token?grant_type=refresh_token&client_id='+ appId +'&refresh_token={refresh_token}');
   var test_bike = new Bike();
   $("#locationForm").submit(function(event){
     event.preventDefault();
     var location = $('#location').val();
+    test_bike.showStolen(location);
     test_bike.getStolen(location);
-    $('#showBikes').text("Here are the bikes in " + location + ":")
-
+    test_bike.justStolen(location);
   })
 
 });
--- js/bike.js
+++ js/bike.js
@@ -3,15 +3,40 @@ var apiKey = require('./../.env').apiKey;
 function Bike() {}
 
 Bike.prototype.getStolen = function(location) {
-  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + location + '&distance=10&stolenness=proximity&appid=' + apiKey).then(function(response) {
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=1000&location=' + location + '&distance=10&stolenness=proximity').then(function(response) {
     console.log(response);
-      debugger;
-    console.log("hello");
+
     $('#showBikes').text("These are the bikes stolen nearby " +location+ ": " + response.bikes.length);
-    debugger;
-    console.log("hello");
 
   });
 };
+
+Bike.prototype.showStolen = function(location) {
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=1000&location=' + location + '&distance=10&stolenness=proximity&appid=' + apiKey).then(function(response) {
+    console.log(response);
+      response.bikes.forEach(function(bike){
+        $('#bikeList').append("<li>" + bike.title + "</li>");
+      });
+
+  });
+};
+
+Bike.prototype.justStolen = function(location) {
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=1000&location=' + location + '&distance=10&stolenness=proximity&appid=' + apiKey).then(function(response) {
+    console.log(response);
+    debugger;
+      var today = new Date();
+      var lastWeek = today.setDate(today.getDate()-15);
+      var lastWeek = lastWeek / 1000;
+      response.bikes.forEach(function(bike){
+        if(bike.date_stolen > lastWeek) {
+          console.log("WAHHT");
+            $('#recentList').append("<li>" + bike.title + " "+ bike.date_stolen + "</li>");
+        }
+      });
+
+  });
+};
+
 
 exports.bikeModule = Bike;
