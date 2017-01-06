7dd657fce748f01087980b4848624dc9033a97db
Cody Brubaker and Marty Kovach <cody@>
2017-01-05 16:03:41 -0800

    hides results after submitting filter form

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -2,33 +2,29 @@
  var Bike = require('./../js/bike.js').bikeModule;
  var appId = require('./../.env').appId;
 $(document).ready(function(){
-  // $.post('https://bikeindex.org/oauth/token?grant_type=refresh_token&client_id='+ appId +'&refresh_token={refresh_token}');
   var test_bike = new Bike();
   $("#locationForm").submit(function(event){
     event.preventDefault();
+    $("#inRange").hide();
     var location = $('#location').val();
     test_bike.showStolen(location);
     test_bike.getStolen(location);
     test_bike.justStolen(location);
-    $("#numOfStolen").toggleClass("hidden");
-    $("#recentlyStolen").toggleClass("hidden");
-    $("#firstHundred").toggleClass("hidden");
-
-    });
+    $("#showBikes").show();
+    $("#bikeList").show();
+    $("#recentList").show();
+  });
 
 
   $('#dateForm').submit(function(event){
     event.preventDefault();
+    $("#showBikes").hide();
+    $("#bikeList").hide();
+    $("#recentList").hide();
     var location = $('#location2').val();
     var startDate = $('#startDate').val();
     var endDate = $('#endDate').val();
     test_bike.showStolenInRange(location, startDate, endDate);
-      $("#searchStolen").toggleClass("hidden");
-    });
-
-
-
-
-
-
+      $("#inRange").show();
+  });
 });
--- js/bike.js
+++ js/bike.js
@@ -6,13 +6,13 @@ Bike.prototype.getStolen = function(location) {
   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=1000&location=' + location + '&distance=10&stolenness=proximity').then(function(response) {
     console.log(response);
 
-    $('#showBikes').text(response.bikes.length);
+    $('#showBikes').append(response.bikes.length);
 
   });
 };
 
 Bike.prototype.showStolen = function(location) {
-  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=1000&location=' + location + '&distance=10&stolenness=proximity&appid=' + apiKey).then(function(response) {
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=1000&location=' + location + '&distance=10&stolenness=proximity').then(function(response) {
     console.log(response);
       response.bikes.forEach(function(bike){
         $('#bikeList').append("<li>" + bike.title + "</li>");
@@ -22,7 +22,7 @@ Bike.prototype.showStolen = function(location) {
 };
 
 Bike.prototype.justStolen = function(location) {
-  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=1000&location=' + location + '&distance=10&stolenness=proximity&appid=' + apiKey).then(function(response) {
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=1000&location=' + location + '&distance=10&stolenness=proximity').then(function(response) {
     console.log(response);
       var today = new Date();
       var lastWeek = today.setDate(today.getDate()-7);
@@ -31,7 +31,7 @@ Bike.prototype.justStolen = function(location) {
       response.bikes.forEach(function(bike){
         if(bike.date_stolen > lastWeek) {
 
-            $('#recentList').append("<li>" + bike.title + " "+ bike.date_stolen + "</li>");
+            $('#recentList').append("<li>" + bike.title + " "+ new Date(bike.date_stolen *1000) + "</li>");
             stolenLastweek.push(bike);
             } if(stolenLastweek.length === 0) {
           $('#recentList').text("Nothing has been stolen in the last seven days");
@@ -41,23 +41,24 @@ Bike.prototype.justStolen = function(location) {
   };
 
 Bike.prototype.showStolenInRange = function(location, startDate, endDate) {
-  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=1000&location=' + location + '&distance=10&stolenness=proximity&appid=' + apiKey).then(function(response) {
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=1000&location=' + location + '&distance=10&stolenness=proximity').then(function(response) {
     console.log(response);
       var start = Date.parse(startDate)/1000;
       var end = Date.parse(endDate)/1000;
-      var stolenLastweek = [];
+      var stolenInRange = [];
       response.bikes.forEach(function(bike){
         if(bike.date_stolen > start && bike.date_stolen < end) {
           if (bike.large_img === null) {
             $('#inRange').append("<h3>" + bike.title + " Date Stolen: "+ new Date(bike.date_stolen *1000) + "<img src='../../img/no-bike-img.png' >" + "</h3>");
           } else {
             $('#inRange').append("<h3>" + bike.title + " Date Stolen: "+ new Date(bike.date_stolen *1000) + "<img src='" +bike.large_img +"' >" + "</h3>");
-            stolenLastweek.push(bike);
-            } if(stolenLastweek.length === 0) {
-          $('#inRange').text("Nothing has been stolen in the last seven days");
-        }
-      }
-    });
+            stolenInRange.push(bike);
+            }
+          }
+        });
+  if (stolenInRange.length === 0) {
+    $('#inRange').append("Nothing has been stolen");
+    }
   });
 };
 
